804fee87008889be660e9fb24a74d7c93b3bcce7
Bodo Schulz <bodo@boone-schulz.de>
2024-08-31 14:20:21 +0200

    update make hooks, molecule and workflows

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,52 +1,112 @@
+from __future__ import unicode_literals
 
 from ansible.parsing.dataloader import DataLoader
 from ansible.template import Templar
+
+import json
 import pytest
 import os
+
 import testinfra.utils.ansible_runner
 
-import pprint
-pp = pprint.PrettyPrinter()
+HOST = 'instance'
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts(HOST)
+
+
+def pp_json(json_thing, sort=True, indents=2):
+    if type(json_thing) is str:
+        print(json.dumps(json.loads(json_thing), sort_keys=sort, indent=indents))
+    else:
+        print(json.dumps(json_thing, sort_keys=sort, indent=indents))
+    return None
 
 
 def base_directory():
+    """
+    """
     cwd = os.getcwd()
-    pp.pprint(cwd)
-    pp.pprint(os.listdir(cwd))
 
-    if ('group_vars' in os.listdir(cwd)):
+    if 'group_vars' in os.listdir(cwd):
         directory = "../.."
         molecule_directory = "."
     else:
         directory = "."
-        molecule_directory = "molecule/{}".format(os.environ.get('MOLECULE_SCENARIO_NAME'))
+        molecule_directory = f"molecule/{os.environ.get('MOLECULE_SCENARIO_NAME')}"
 
     return directory, molecule_directory
+
+
+def read_ansible_yaml(file_name, role_name):
+    """
+    """
+    read_file = None
+
+    for e in ["yml", "yaml"]:
+        test_file = f"{file_name}.{e}"
+        # print(f" -> {test_file}")
+        if os.path.isfile(test_file):
+            read_file = test_file
+            break
+        else:
+            print(f"E: miss file {test_file}")
+
+    return f"file={read_file} name={role_name}"
 
 
 @pytest.fixture()
 def get_vars(host):
     """
+        parse ansible variables
+        - defaults/main.yml
+        - vars/main.yml
+        - vars/${DISTRIBUTION}.yaml
+        - molecule/${MOLECULE_SCENARIO_NAME}/group_vars/all/vars.yml
 
+        - 'MOLECULE_PROJECT_DIRECTORY': '/src/ansible/ansible-senchacmd'
     """
     base_dir, molecule_dir = base_directory()
+    distribution = host.system_info.distribution
+    operation_system = None
 
-    pp.pprint(" => '{}' / '{}'".format(base_dir, molecule_dir))
+    if distribution in ['debian', 'ubuntu']:
+        operation_system = "debian"
+    elif distribution in ['redhat', 'ol', 'centos', 'rocky', 'almalinux']:
+        operation_system = "redhat"
+    elif distribution in ['arch', 'artix']:
+        operation_system = f"{distribution}linux"
 
-    file_defaults = "file={}/defaults/main.yml name=role_defaults".format(base_dir)
-    file_vars = "file={}/vars/main.yml name=role_vars".format(base_dir)
-    file_molecule = "file={}/group_vars/all/vars.yml name=test_vars".format(molecule_dir)
+    project_directory = os.environ.get("MOLECULE_PROJECT_DIRECTORY")
+    os.chdir(project_directory)
 
-    defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
-    vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
-    molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+    # print(f" -> {distribution} / {operation_system}")
+    # # print(f" -> {base_dir} / {molecule_dir}")
+    # print(f" -> {os.getcwd()}")
+    # print(f" -> {os.environ}")
+
+    file_defaults      = read_ansible_yaml(f"{base_dir}/defaults/main", "role_defaults")
+    file_vars          = read_ansible_yaml(f"{base_dir}/vars/main", "role_vars")
+    file_distibution   = read_ansible_yaml(f"{base_dir}/vars/{operation_system}", "role_distibution")
+    file_molecule      = read_ansible_yaml(f"{molecule_dir}/group_vars/all/vars", "test_vars")
+    # file_host_molecule = read_ansible_yaml("{}/host_vars/{}/vars".format(base_dir, HOST), "host_vars")
+
+    # print(f" -> {file_defaults}")
+    # print(f" -> {file_vars}")
+    # print(f" -> {file_distibution}")
+    # print(f" -> {file_molecule}")
+
+    defaults_vars      = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
+    vars_vars          = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    distibution_vars   = host.ansible("include_vars", file_distibution).get("ansible_facts").get("role_distibution")
+    molecule_vars      = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+    # host_vars          = host.ansible("include_vars", file_host_molecule).get("ansible_facts").get("host_vars")
 
     ansible_vars = defaults_vars
     ansible_vars.update(vars_vars)
+    ansible_vars.update(distibution_vars)
     ansible_vars.update(molecule_vars)
+    # ansible_vars.update(host_vars)
 
     templar = Templar(loader=DataLoader(), variables=ansible_vars)
     result = templar.template(ansible_vars, fail_on_undefined=False)
@@ -75,15 +135,18 @@ def test_files(host, files):
 
 
 def test_installation_directory(host, get_vars):
-    vers = get_vars['sencha_version']
 
-    directory = "/opt/Sencha/Cmd/{}".format(vers)
+    print(get_vars)
+
+    vers = get_vars.get('sencha_version', None)
+
+    directory = f"/opt/Sencha/Cmd/{vers}"
 
     d = host.file(directory)
     assert d.is_directory
     assert d.exists
 
     for file in ["sencha.jar", "sencha.vmoptions"]:
-        f = host.file("{}/{}".format(directory, file))
+        f = host.file(f"{directory}/{file}")
         assert f.exists
         assert f.is_file
